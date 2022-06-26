2474e461db300ec163df76caa050af3afe77218a
Suryakanth Reddy <suryakanthreddy@Suryakanths-MacBook-Pro.local>
2022-06-26 18:59:17 +0530

    added comment

--- frontend-Todo/src/Components/to-do/Welcome.js
+++ frontend-Todo/src/Components/to-do/Welcome.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Image from "../../assest/Todo.jpeg"
+import Image from "../../assest/Todo.jpeg";
 import classes from "./Welcome.module.css";
 import Card from "../UI/Card";
-
+// welcome screen
 const Welcome = () => {
   return (
-    <Card >
-    <div className={classes.main}>
-    <h3 >Welcome Back!</h3>
-    <img src={Image} alt=""></img>
-    <div className={classes.btn1}>
-      <Link to="/login">
-      <button type="button" class="btn p-2  btn-dark">Login</button>
-      </Link>
-      <Link to="/signup">
-      <button type="button" class="btn  p-2 btn-dark">Signup</button>
-      </Link>
+    <Card>
+      <div className={classes.main}>
+        <h3>Welcome Back!</h3>
+        <img src={Image} alt=""></img>
+        <div className={classes.btn1}>
+          <Link to="/login">
+            <button type="button" class="btn p-2  btn-dark">
+              Login
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button type="button" class="btn  p-2 btn-dark">
+              Signup
+            </button>
+          </Link>
+        </div>
       </div>
-      </div>
-      
     </Card>
   );
 };
